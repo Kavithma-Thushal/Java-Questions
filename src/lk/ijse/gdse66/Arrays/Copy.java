6b594e58994e7554d5f4b1ea21eb48ea736957b7
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-26 14:28:06 +0530

    Copy Array

---
+++ src/lk/ijse/gdse66/Arrays/Copy.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse66.Arrays;
+
+import java.util.Arrays;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Java-Questions
+ * @since : 2:26 PM - 12/26/2023
+ **/
+public class Copy {
+    public static void main(String[] args) {
+        int[] arr = {12, 23, 34, 45, 56};
+        int[] newArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(newArr));    // [12, 23, 34, 45, 56]
+    }
+}

902b4d83f893f8a8e3d67bf5932f4ec50848ee28
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-26 15:47:21 +0530

    Digit To Array

---
+++ src/lk/ijse/gdse66/Arrays/DigitToArray.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse66.Arrays;
+
+import java.util.Arrays;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Java-Questions
+ * @since : 2:42 PM - 12/26/2023
+ **/
+public class DigitToArray {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int num = 34678;
+
+        for (int i = 0; i < arr.length; i++) {
+            int save = num % 10;
+            num /= 10;
+            arr[arr.length - (i + 1)] = save;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}

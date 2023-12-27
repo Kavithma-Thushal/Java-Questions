c2c1c719d8bc6ff2e972bda9d27a77d9a69b1008
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-27 17:25:43 +0530

    Refactor

--- src/lk/ijse/gdse66/Others/Sample.java
+++ src/lk/ijse/gdse66/Others/Sample.java
@@ -1,7 +1,5 @@
 package lk.ijse.gdse66.Others;
 
-import java.util.Scanner;
-
 /**
  * @author : Kavithma Thushal
  * @project : Java-Questions
@@ -9,36 +7,6 @@ import java.util.Scanner;
  **/
 public class Sample {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Input a number: ");
-        int num = input.nextInt();
-
-        int temp = num;
-        int temp2 = num;
-        int count = 0;
-        int result = 0;
-
-        while (temp != 0) {
-            temp /= 10;
-            count++;
-        }
-
-        while (temp2 != 0) {
-            int remain = temp2 % 10;    //1
-            int t = 1;
-            for (int i = 0; i < count; i++) {
-                t *= remain;
-            }
-            result += t;
-            t = 1;
-
-            temp2 /= 10;
-        }
-        if (num == result) {
-            System.out.println(num + " is a armstrong number");
-        } else {
-            System.out.println(num + " is not a armstrong number");
-        }
     }
 }

214d9bff1b78464ad632657d49610c581d795149
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-28 20:32:50 +0530

    HeapSort

---
+++ src/lk/ijse/gdse66/Algorithms/HeapSort.java
@@ -0,0 +1,65 @@
+package lk.ijse.gdse66.Algorithms;
+
+import java.util.Arrays;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Java-Questions
+ * @since : 8:30 PM - 12/28/2023
+ **/
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] array = {8, 9, 7, 1, 3, 2};
+        System.out.println("Original Array : " + Arrays.toString(array));
+
+        heapSort(array);
+        System.out.println("Sorted Array   : " + Arrays.toString(array));
+    }
+
+    public static void heapSort(int[] array) {
+        int n = array.length;
+
+        // Build a max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+
+        // Extract elements from the heap one by one
+        for (int i = n - 1; i >= 0; i--) {
+            // Swap the root (maximum element) with the last element
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Call heapify on the reduced heap
+            heapify(array, i, 0);
+        }
+    }
+
+    public static void heapify(int[] array, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int leftChild = 2 * i + 1; // Left child
+        int rightChild = 2 * i + 2; // Right child
+
+        // If left child is larger than root
+        if (leftChild < n && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+
+        // If right child is larger than largest so far
+        if (rightChild < n && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        // If largest is not the root
+        if (largest != i) {
+            // Swap the root with the largest element
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+
+            // Recursively heapify the affected sub-tree
+            heapify(array, n, largest);
+        }
+    }
+}

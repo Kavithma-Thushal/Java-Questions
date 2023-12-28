5fc520092633973e6259fbd2c7a386ec2f75417e
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-28 22:10:23 +0530

    LinearSearch

---
+++ src/lk/ijse/gdse66/Algorithms/LinearSearch.java
@@ -0,0 +1,30 @@
+package lk.ijse.gdse66.Algorithms;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Java-Questions
+ * @since : 9:59 PM - 12/28/2023
+ **/
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 7;
+
+        int result = linearSearch(array, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; // Target found at index i
+            }
+        }
+        return -1; // Target not found in the array
+    }
+}

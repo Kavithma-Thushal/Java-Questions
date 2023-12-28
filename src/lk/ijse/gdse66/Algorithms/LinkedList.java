2978938da97ba06bb02a77e8abcdca567d0023f9
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-28 22:25:39 +0530

    LinkedList

---
+++ src/lk/ijse/gdse66/Algorithms/LinkedList.java
@@ -0,0 +1,55 @@
+package lk.ijse.gdse66.Algorithms;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Java-Questions
+ * @since : 10:23 PM - 12/28/2023
+ **/
+public class LinkedList {
+    private Node head;
+
+    private static class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Add a new node to the end of the linked list
+    public void append(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+    }
+
+    // Print the linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList myList = new LinkedList();
+
+        myList.append("Alice");
+        myList.append("Bob");
+        myList.append("Charlie");
+
+        System.out.print("Linked List : ");
+        myList.display();
+    }
+}

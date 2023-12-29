039def56298b44bb5281757636c004d09562ead1
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-30 00:38:11 +0530

    2D read

--- src/lk/ijse/gdse66/Arrays/Arrays2D/Intro.java
+++ src/lk/ijse/gdse66/Arrays/Arrays2D/Intro.java
@@ -9,8 +9,9 @@ import java.util.Arrays;
  **/
 public class Intro {
     public static void main(String[] args) {
-        create();
-        create2();
+        //create();
+        //create2();
+        //read();
     }
 
     public static void create() {
@@ -21,5 +22,25 @@ public class Intro {
     public static void create2() {
         int[][] arr = {{5, 3}, {7, 2}, {4, 8}, {2, 5}, {1, 7}};
         System.out.println(Arrays.deepToString(arr));
+    }
+
+    public static void read() {
+        int[][] arr = new int[5][2];
+
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+                if (j < arr[i].length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.print("]");
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
     }
 }

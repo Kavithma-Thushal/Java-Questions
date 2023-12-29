ff3843784e8dc8a5dd753794ba433ce0aace1495
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-29 23:49:59 +0530

    2D Create

--- src/lk/ijse/gdse66/Arrays/Arrays2D/Intro.java
+++ src/lk/ijse/gdse66/Arrays/Arrays2D/Intro.java
@@ -9,8 +9,17 @@ import java.util.Arrays;
  **/
 public class Intro {
     public static void main(String[] args) {
-        int[][] arr = new int[5][2];
+        create();
+        create2();
+    }
 
+    public static void create() {
+        int[][] arr = new int[5][2];
+        System.out.println(Arrays.deepToString(arr));
+    }
+
+    public static void create2() {
+        int[][] arr = {{5, 3}, {7, 2}, {4, 8}, {2, 5}, {1, 7}};
         System.out.println(Arrays.deepToString(arr));
     }
 }

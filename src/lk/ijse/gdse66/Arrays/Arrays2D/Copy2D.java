04e13b1f5f7e6ed79722a00490aaa9ca679c407e
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-12-30 09:15:38 +0530

    Copy2D Implemented

--- src/lk/ijse/gdse66/Arrays/Arrays2D/Copy2D.java
+++ src/lk/ijse/gdse66/Arrays/Arrays2D/Copy2D.java
@@ -12,7 +12,13 @@ public class Copy2D {
         int[][] arr = {{5, 3}, {7, 2}, {4, 8}, {2, 5}, {1, 7}};
         int[][] newArr = new int[arr.length][arr[0].length];
 
-        newArr = Arrays.copyOf(arr, arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                newArr[i][j] = arr[i][j];
+            }
+        }
+
+        //newArr = Arrays.copyOf(arr, arr.length);
         System.out.println(Arrays.deepToString(newArr));
     }
 }

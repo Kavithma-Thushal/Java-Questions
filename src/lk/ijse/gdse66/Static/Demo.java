e6019f31a6ae77e70deb61f58edeaac4c8c9e661
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-01-03 15:55:07 +0530

    Static Added

---
+++ src/lk/ijse/gdse66/Static/Demo.java
@@ -0,0 +1,41 @@
+package lk.ijse.gdse66.Static;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Java-Questions
+ * @since : 3:52 PM - 1/3/2024
+ **/
+class Employee {
+    String id;
+    double salary;
+    static String ceoName;
+
+    public void printDetails() {
+        System.out.println(id + " : " + salary + " : " + ceoName);
+    }
+}
+
+public class Demo {
+    public static void main(String args[]) {
+        Employee.ceoName = "Thushal";
+
+        Employee sahan = new Employee();
+        sahan.id = "E001";
+        sahan.salary = 1000;
+        sahan.ceoName = "Kamal";
+
+        Employee nimna = new Employee();
+        nimna.id = "E002";
+        nimna.salary = 500;
+        nimna.ceoName = "Nimal";
+
+        Employee udara = new Employee();
+        udara.id = "E003";
+        udara.salary = 100000;
+        udara.ceoName = "Jagath";
+
+        sahan.printDetails();
+        nimna.printDetails();
+        udara.printDetails();
+    }
+}
